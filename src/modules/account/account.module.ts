57253d2a1aa2feadecd82be65ee43ae93e386efe
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2019-12-31 11:36:23 -0300

    Remove context configuration, it's only a test

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,16 +1,11 @@
-import { GraphQLModule } from '@graphql-modules/core';
-import { buildSchemaSync } from 'type-graphql';
-import CustomerResolver from '../account/resolvers/customer.resolvers';
 import AddressResolver from './resolvers/address.resolvers';
 import CityResolver from './resolvers/city.resolvers';
 import CountryResolver from './resolvers/country.resolvers';
+import CustomerResolver from '../account/resolvers/customer.resolvers';
+import { GraphQLModule } from '@graphql-modules/core';
+import { buildSchemaSync } from 'type-graphql';
 
 export const AccountModule = new GraphQLModule({
-  context: ({ req }) => {
-    const token = req.get('Authorization');
-    console.log(token);
-    return { accoun: 'teste' };
-  },
   extraSchemas: [
     buildSchemaSync({
       resolvers: [CustomerResolver, AddressResolver, CityResolver, CountryResolver],
