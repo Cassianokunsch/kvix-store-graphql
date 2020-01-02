09bd8366dee6042b9e42a9984c9097c5f84dd299
cassiano.kunsch <cassiano.kunsch@outlook.com>
2020-01-02 01:27:19 -0300

    Insert a new role

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
         sourceType: 'module', // Allows for the use of imports
     },
     rules: {
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     },
