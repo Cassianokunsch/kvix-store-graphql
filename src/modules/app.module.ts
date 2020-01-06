a11c8c3752a2bea67e28c3e8b2f2bf82d6c82a32
cassiano.kunsch <cassiano.kunsch@outlook.com>
2020-01-06 00:03:07 -0300

    Import new module - product

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,8 @@
 import { GraphQLModule } from '@graphql-modules/core';
 
 import { AccountModule } from './account/account.module';
+import { ProductModule } from './product/product.module';
 
 export const AppModule = new GraphQLModule({
-  imports: [AccountModule],
+  imports: [AccountModule, ProductModule],
 });
