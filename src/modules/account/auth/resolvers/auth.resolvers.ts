9656082763696f63f818f9698e065fd2634bfdf7
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-01-13 18:36:40 -0300

    Adding new logic to singUp

--- src/modules/account/auth/resolvers/auth.resolvers.ts
+++ src/modules/account/auth/resolvers/auth.resolvers.ts
@@ -7,6 +7,7 @@ import { sign } from 'jsonwebtoken';
 import { errorName } from '../../../shared/errors';
 import { User } from '../entities/User';
 import { AuthInput } from './inputs/auth.input';
+import { SignUpInput } from './inputs/signUp.input';
 import { Payload } from './types/payload.type';
 
 @Resolver()
@@ -25,11 +26,23 @@ export class AuthResolver {
   }
 
   @Mutation(() => Payload)
-  async signUp(@Arg('authInput') { email, password }: AuthInput): Promise<Payload> {
-    const existUser = await getRepository(User).findOne({ where: { email } });
-    if (existUser) throw new Error(errorName.EMAIL_ALREADY_USE);
+  async signUp(@Arg('signUpInput') signUpInput: SignUpInput): Promise<Payload> {
+    const { email, password, cellPhone, cpf, name } = signUpInput;
 
-    const userToCreate = getRepository(User).create({ email, password });
+    const queryResult = await getRepository(User)
+      .createQueryBuilder('user')
+      .where('user.cpf = :cpf OR user.cell_phone = :cellPhone OR user.email = :email', { cpf, cellPhone, email })
+      .getOne();
+
+    if (queryResult) {
+      if (queryResult.cpf == cpf) throw Error('CPF is already in use!');
+
+      if (queryResult.cellPhone == cellPhone) throw Error('Cellphone is already in use!');
+
+      if (queryResult.email == email) throw new Error(errorName.EMAIL_ALREADY_USE);
+    }
+
+    const userToCreate = getRepository(User).create({ email, password, cellPhone, cpf, name });
     const user = await getRepository(User).save(userToCreate);
 
     if (!process.env.APP_SECRET) throw Error('Erro to get APP_SECRET');
