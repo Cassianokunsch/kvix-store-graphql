2815e5fd987436c9320c82b6e39f1478a833c840
cassiano.kunsch <cassiano.kunsch@outlook.com>
2020-01-16 00:55:59 -0300

    Create new files to represent service layer

---
+++ src/modules/account/customer/services/AddressService.ts
@@ -0,0 +1,34 @@
+import { Repository, getRepository } from 'typeorm';
+
+import { Address } from '../entities/Address';
+import { City } from '../entities/City';
+
+export class AddressService {
+  private _addressRepository: Repository<Address> = getRepository(Address);
+
+  async getAllAddresses(): Promise<Address[]> {
+    return await this._addressRepository.find();
+  }
+
+  async createAddress(cityId: string, neighborhood: string, number: string, street: string, customerId: string, complement?: string): Promise<Address> {
+    const address = this._addressRepository.create({
+      neighborhood,
+      number,
+      street,
+      complement,
+      customer: {
+        id: customerId,
+      },
+      city: {
+        id: cityId,
+      },
+    });
+
+    return await this._addressRepository.save(address);
+  }
+
+  async getFieldResolverCity(addressId: string): Promise<City> {
+    const { city } = await this._addressRepository.findOneOrFail({ where: { id: addressId }, relations: ['city'] });
+    return city;
+  }
+}
