fa9ed184f74fb44472a23a608a1161189552e0b9
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-01-17 12:09:00 -0300

    Remove import imagemResolver

--- src/modules/product/resolvers/index.ts
+++ src/modules/product/resolvers/index.ts
@@ -1,4 +1,3 @@
 export { BrandResolver } from './BrandResolver';
 export { CategoryResolver } from './CategoryResolver';
-export { ImageResolver } from './ImageResolver';
 export { ProductResolver } from './ProductResolver';
