2de52e2baae58e7f1622176976960e7366a25d2f
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-05-04 23:13:27 -0300

    brand service test

--- src/product/services/__tests__/data.mock.ts
+++ src/product/services/__tests__/data.mock.ts
@@ -1,8 +1,14 @@
-export const product = {
+export const productMock = {
   id: 'acd82bac-d7ce-4524-9a45-4c9cbce7dcb8',
   name: 'Bolsa',
   description: 'Linda bolsa',
   price: 150.0,
 };
 
-export const lstOfProduct = [product, product, product];
+export const brandMock = {
+  id: 'acd82bac-d7ce-4524-9a45-4c9cbce7dcb8',
+  name: 'Kvix',
+};
+
+export const lstOfProduct = [productMock, productMock, productMock];
+export const lstOfBrand = [brandMock, brandMock, brandMock];
