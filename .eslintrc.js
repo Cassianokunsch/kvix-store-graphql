e32f76b16803da2d7df3ba10476e78d988f61448
Cassiano Kunsch das Neves <cassiano.neves@avista.local>
2020-05-25 21:33:41 -0300

    Colocando novas regras e plugins de organizacao de import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,27 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-        //'plugin:eslint-plugin-import-helpers'
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import-helpers'],
+  extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'prettier/@typescript-eslint'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [['/^@nest/', '/^sequelize/'], 'module', '/^@shared/', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
     ],
-    parserOptions: {
-        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
-    },
-    plugins: ['eslint-plugin-import-helpers'],
-
-    rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        'import-helpers/order-imports': [
-            'warn',
-            { // example configuration
-                newlinesBetween: 'always',
-                groups: [
-                    ['/^graphql/', '/^type/'],
-                    'module',
-                    '/^@shared/',
-                    ['parent', 'sibling', 'index'],
-                ],
-                alphabetize: { order: 'asc', ignoreCase: true },
-            },
-        ],
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    },
-  };
-  
+  },
+};
